d598d1440c7229e4675d6986dfe8efa82a709444
Vishnu M <vyshnav.m345@gmail.com>
2023-07-07 12:15:44 +0530

    Make The cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -71,9 +71,12 @@ products.forEach((product) => {
                 quantity: 1
             });
         }
-        
-        console.log(cart);
-    })
-    
+         let cartQuantity = 0;
+         cart.forEach((item) => {
+            cartQuantity+=item.quantity;
+         });
+
+         document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    })   
  })
  
